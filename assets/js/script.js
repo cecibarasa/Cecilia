ffa7b546301bdbee7f4ddd794e6e558840616ac7
Cecilia Barasa <cecibarasa@gmail.com>
2021-01-02 15:15:30 +0300

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-126939217-2');
+
+$(document).ready(function() {
+    $(".scrollspy").scrollSpy()
+        // Initialize collapse button
+    $(".button-collapse").sideNav({
+        menuWidth: 190, // Default is 240
+        edge: "left", // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    })
+})
