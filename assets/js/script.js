cff18f339fedd3611ead2f2485ce8e30ab1aacac
Cecilia Barasa <cecibarasa@gmail.com>
2021-01-02 15:54:24 +0300

    added resume link

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,22 @@ $(document).ready(function() {
         closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     })
 })
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
